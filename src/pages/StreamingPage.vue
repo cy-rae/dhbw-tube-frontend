2039183de159f2e6826e5ca334c0ddc8f5b17ff0
NDI6FE <dario.nieddu@de.bosch.com>
2024-08-06 16:12:43 +0200

    Adapted styles

--- src/pages/StreamingPage.vue
+++ src/pages/StreamingPage.vue
@@ -1,11 +1,13 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <div class="col-8">
-      <q-video
-        :ratio="16/9"
-        src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"
-      />
-    </div>
+  <q-page class="row items-center justify-evenly q-pa-xl">
+    <q-scroll-area style="width: 100vh; height: 100vw">
+      <div class="col">
+        <q-video
+          :ratio="16/9"
+          src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"
+        />
+      </div>
+    </q-scroll-area>
   </q-page>
 </template>
 
