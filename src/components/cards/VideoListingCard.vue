5fa0a7bda78612fdb854c0bb634e3e4772a13c2a
Dario Nieddu <cy.rae@yahoo.com>
2024-08-07 21:13:22 +0200

    adapted search page

--- src/components/cards/VideoListingCard.vue
+++ src/components/cards/VideoListingCard.vue
@@ -1,20 +1,24 @@
 <template>
-  <q-card class="video-listing-card">
-    <q-card-section horizontal class="q-pa-none">
+  <q-card
+    @click="onVideoListing"
+    v-ripple
+    class="video-listing-card full-width cursor-pointer q-hoverable"
+  >
+    <q-card-section horizontal class="q-pa-none q-pl-md">
       <q-card-section class="row justify-evenly items-center q-pa-none" style="width: 250px">
         <img
           :src="props.videoListing.coverBase64"
           :alt="$t('search.cover-alt')"
-          class="q-px-auto"
-          style="height: 150px"
+          class="q-px-auto curved-border"
+          style="max-height: 150px; max-width: 250px"
         />
       </q-card-section>
 
       <q-separator vertical />
 
-      <q-card-section class="q-pa-none">
-        {{ props.videoListing.title }}
-        {{ props.videoListing.username }}
+      <q-card-section class="q-pa-none q-my-auto q-pl-md">
+        <div class="row text-h5">{{ props.videoListing.title }}</div>
+        <div class="row text-h6">{{ props.videoListing.username }}</div>
       </q-card-section>
     </q-card-section>
   </q-card>
@@ -22,13 +26,22 @@
 
 <script setup lang="ts">
 import { VideoListingDTO } from 'src/dtos/VideoListingDTO';
+import { useRouter } from 'vue-router';
+import { RoutePaths } from 'src/enums/RoutePaths';
 
+// Helpers
+const router = useRouter();
+
+// Properties
 interface Props {
   videoListing: VideoListingDTO;
 }
 
 const props = defineProps<Props>();
 
+function onVideoListing() {
+  router.push(RoutePaths.STREAMING_PAGE);
+}
 </script>
 
 <style>
